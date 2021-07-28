0a4072e1bce1f8e5fc8feb8f2fb4a68d3ba5aeae
MattWVile <matt.vile@yahoo.co.uk>
2021-07-28 10:39:55 +0000

    added scripts

---
+++ scripts/fileexist.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+echo "What file do you want to find?"
+read filename
+where=$(pwd)
+cd /home/$(whoami)
+value=$(find "$(cd ..; pwd)" -name ${filename} | head -n 1)
+
+if [ ${value} ] 
+then
+    echo "Exists ${value}"
+elif [ -d ${value} ]
+then
+    echo 'elif'
+    echo "not Exists ${value}"
+else
+    echo "Does not exist"
+fi
